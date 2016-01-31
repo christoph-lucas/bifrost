d6f4ca17f9978561f37f32a425475af548363073
Christoph Lucas <christoph.lucas@gmail.com>
2016-01-31 12:52:10 +0100

    Setup Project for Integration Tests

---
+++ integration-tests/src/test/java/ch/bifrost/integrationtest/datagram/EndpointCommunicationTest.java
@@ -0,0 +1,10 @@
+package ch.bifrost.integrationtest.datagram;
+
+public class EndpointCommunicationTest {
+
+//	@Test
+//	public void shouldFail() throws Exception {
+//		throw new Exception();
+//	}
+	
+}
