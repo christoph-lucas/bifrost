ae9ce5fd4bcbfa6e9696e7ad0aa3cd6e4fb27aab
Christoph Lucas <christoph.lucas@gmail.com>
2016-02-25 22:58:14 +0100

    Added slf4j logging framework

--- core/src/main/java/ch/bifrost/core/Hello.java
+++ core/src/main/java/ch/bifrost/core/Hello.java
@@ -1,7 +1,11 @@
 package ch.bifrost.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Hello {
 	public static void main (String[] args) {
-		System.out.println("hello there");
+	    Logger logger = LoggerFactory.getLogger(Hello.class);
+	    logger.info("Hello World");
 	}
 }
