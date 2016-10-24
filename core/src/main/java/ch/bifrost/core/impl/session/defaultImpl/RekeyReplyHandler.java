0eead391740829aa0799bfedc3488315643f7417
Christoph Lucas <christoph.lucas@gmail.com>
2016-10-24 20:01:42 +0200

    #33 Simply renaming some classes

--- core/src/main/java/ch/bifrost/core/impl/session/defaultImpl/RekeyReplyHandler.java
+++ core/src/main/java/ch/bifrost/core/impl/session/defaultImpl/RekeyReplyHandler.java
@@ -8,12 +8,12 @@ import ch.bifrost.core.impl.MessageCodecUtils;
 /**
  * Handles Rekey Reply Messages.
  */
-public class RekeyReplyHandler implements DefaultSessionLayerMessageHandler {
+public class RekeyReplyHandler implements MessageHandler {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RekeyReplyHandler.class);
 
 	@Override
-	public void handle (DefaultSessionLayerMessage message) {
+	public void handle (Message message) {
 		LOG.debug("Received rekey reply with content: " + MessageCodecUtils.decodeStringFromByteArray(message.getPayload()));
 	}
 
